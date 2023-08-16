c0ddab1a0d6b64db16c37588a8ffa4b5c1cb8500
Uliana.29 <Uliana.29@DESKTOP-6DD5K9D>
2023-08-16 17:08:06 +0300

    Add project files.

---
+++ DataContainers/main.cpp
@@ -0,0 +1,181 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#define tab "\t"
+
+class ForwardList;
+ForwardList operator+(const ForwardList& left, const ForwardList& right);
+
+class Element
+{
+	int Data;
+	Element* pNext;
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
+	friend class ForwardList;
+};
+class ForwardList
+{
+	Element* Head;
+public:
+	ForwardList()
+	{
+		Head = nullptr;
+		cout << "LConstructor" << this << endl;
+	}
+	ForwardList(const ForwardList& other)
+	{
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LConstructor: \t" << this << endl;
+	}
+	~ForwardList()
+	{
+		while (Head)pop_front();
+		cout << "LDestructor:\t" << this << endl;
+	}	
+
+	//				Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_back(Temp->Data);
+		cout << "LConstructor: \t" << this << endl;
+		return *this;
+	}
+
+
+	//					Adding element:
+	void push_front(int Data)
+	{
+		Head = new Element(Data, Head);
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return push_front(Data);				
+		Element* Temp = Head;
+		while (Temp->pNext)
+			Temp = Temp->pNext;
+		Temp->pNext = new Element(Data);
+	}
+	//					Delete elements:
+	void pop_front()
+	{
+		Element* erased = Head;
+		Head = Head->pNext;
+		delete erased;
+	}
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)Temp = Temp->pNext;
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+	}
+	void erase(int Index)
+	{
+		if (Index == 0)return pop_front();
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)
+			if (Temp->pNext)Temp = Temp->pNext;
+		Element* erased = Temp->pNext;
+		Temp->pNext = Temp->pNext->pNext;
+		delete erased;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index == 0)return push_front(Data);
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)
+			if (Temp->pNext)
+				Temp = Temp->pNext;
+		Element* New = new Element(Data);
+		New->pNext = Temp->pNext;
+		Temp->pNext = New;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		/*Element* Temp = Head;
+		while (Temp)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;
+		}*/
+		cout << "Head: " << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+	}	
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
+};
+
+ForwardList operator+(const ForwardList& left, const ForwardList& right)
+{
+	ForwardList cat = left;
+	for (Element* Temp = right.Head; Temp; Temp = Temp->pNext)cat.push_back(Temp->Data);
+	return cat;
+}
+
+//#define BASE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef BASE_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	ForwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		//list.push_front(rand() % 100);
+		list.push_back(rand() % 100);
+	}
+	//list.push_back(123);
+	list = list;
+	list.print();
+
+	/*int value;
+	int index;
+	cout << "Введите индех элемента: "; cin >> index;
+	cout << "Введите значение элемента: "; cin >> value;
+	list.insert(value, index);
+	list.print();
+
+	cout << "Введите индех элемента: "; cin >> index;
+	list.erase(index);
+	list.print();*/
+
+	//ForwardList list2 = list;
+
+	ForwardList list2;
+	list2 = list;		//Copy assigment
+	list2.print();
+#endif // BASE_CHECK
+
+	ForwardList list1;
+	list1.push_back(3);
+	list1.push_back(5);
+	list1.push_back(8);
+	list1.push_back(13);
+	list1.push_back(21);
+	list1.print();
+	
+	ForwardList list2;
+	list2.push_back(34);
+	list2.push_back(55);
+	list2.push_back(89);
+	list2.print();
+}
